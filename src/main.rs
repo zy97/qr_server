1cde9b540b3bacc5d2d56e7cda165deb93f3209a
bomky <bom_ky@outlook.com>
2024-12-12 16:56:54 +0800

    完成真正的模板渲染

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,9 @@ use headless_chrome::{protocol::cdp::Page, Browser, Tab};
 use image::{ImageFormat, Luma};
 use lazy_static::lazy_static;
 use qrcode::QrCode;
-use resvg::{tiny_skia, usvg};
 use serde::{Deserialize, Serialize};
-use std::{env, fs::File, io::Cursor, sync::Arc};
+use std::{env, fs::File, io::Cursor, process::Command, sync::Arc};
 use tera::{Context, Tera};
-use tracing::info;
 use tracing_subscriber::FmtSubscriber;
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
@@ -50,38 +48,52 @@ async fn get_qr_code(qr_code: web::Path<String>) -> Result<impl Responder, Custo
 }
 #[post("/label")]
 async fn create_label(labels: web::Json<Vec<LabelInfo>>) -> Result<impl Responder, CustomError> {
-    info!("1");
-    info!("requests: {:#?}", labels);
-
-    let code = QrCode::new(r"qr_code.as_bytes()")?;
-    let image = code.render::<Luma<u8>>().build();
-    image.save("./templates/qr.png")?;
-    let mut result = File::create("./templates/result.html")?;
-    TEMPLATES.render_to(
-        "template.html",
-        &Context::from_serialize(&labels)?,
-        &mut result,
-    )?;
     let tab = CTAB.clone();
-    info!("2");
-    let current_dir = env::current_dir()?;
-    let file_path = current_dir.join("templates/result.html");
-    let viewport = tab
-        .navigate_to(&format!("file:///{}", file_path.display()))?
-        .wait_for_element("table")?
-        .get_box_model()?
-        .margin_viewport();
-    let jpeg_data = tab.capture_screenshot(
-        Page::CaptureScreenshotFormatOption::Png,
-        Some(75),
-        Some(viewport),
-        true,
-    )?;
-    info!("3");
-    std::fs::write("screenshot.png", jpeg_data)?;
-    Ok(NamedFile::open("screenshot.png")?)
+    for label in labels.0 {
+        let code = QrCode::new(&label.qr_code)?;
+        let infos = split_info(&label.qr_code);
+        let image = code.render::<Luma<u8>>().build();
+        image.save("./templates/qr.png")?;
+        let mut result = File::create("./templates/result.html")?;
+        TEMPLATES.render_to(
+            "template.html",
+            &Context::from_serialize(&infos)?,
+            &mut result,
+        )?;
+
+        let current_dir = env::current_dir()?;
+        let file_path = current_dir.join("templates/result.html");
+        let viewport = tab
+            .navigate_to(&format!("file:///{}", file_path.display()))?
+            .wait_for_element("table")?
+            .get_box_model()?
+            .margin_viewport();
+        let jpeg_data = tab.capture_screenshot(
+            Page::CaptureScreenshotFormatOption::Png,
+            Some(75),
+            Some(viewport),
+            true,
+        )?;
+        std::fs::write("result.png", jpeg_data)?;
+        Command::new(r".\printer.exe")
+            .args(&["result.png"])
+            .output()?;
+    }
+    Ok(NamedFile::open("result.png")?)
 }
 
+fn split_info(code: &str) -> TemplateData {
+    let infos = code.split('|').collect::<Vec<&str>>();
+    TemplateData {
+        material_no: infos[0].to_string(),
+        lot_no: infos[1].to_string(),
+        order_no: infos[2].to_string(),
+        count: infos[3].to_string(),
+        vender_code: infos[4].to_string(),
+        date: infos[5].to_string(),
+        box_no: infos[6].to_string(),
+    }
+}
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = FmtSubscriber::builder()
@@ -105,21 +117,25 @@ async fn main() -> std::io::Result<()> {
 struct LabelInfo {
     /// 类型：1：半成品，2：成品
     kind: i32,
-
     /// 订单号
     order_no: String,
-
     /// 客户名称
     customer_name: String,
-
     /// 型号
     product_model: String,
-
     /// 品名
     commodity: String,
-
     /// 二维码
     qr_code: String,
-
     is_return: bool,
+}
+#[derive(Serialize)]
+struct TemplateData {
+    material_no: String,
+    lot_no: String,
+    order_no: String,
+    count: String,
+    vender_code: String,
+    date: String,
+    box_no: String,
 }
